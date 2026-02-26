8672fc3d3f3b53e7e17aa7e9bd7c869d2ea8c621
Chris Nicholas <ctnicholasdev@gmail.com>
2026-02-26 11:58:54 +0000

    feat: Liveblocks style code snippets

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,11 +1,30 @@
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import {
+  CodeBlock,
+  CodeBlockTab,
+  CodeBlockTabs,
+  CodeBlockTabsList,
+  CodeBlockTabsTrigger,
+  Pre,
+} from "./components/codeblock";
+import { Tabs } from "./components/tabs";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     img: (props) => <ImageZoom {...(props as any)} />,
+    CodeBlock,
+    CodeBlockTab,
+    CodeBlockTabs,
+    CodeBlockTabsList,
+    CodeBlockTabsTrigger,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ...components,
   };
 }
